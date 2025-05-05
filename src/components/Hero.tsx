a0a2652657c4458921a7f9b5a8f5b5de71ca2dcb
cjmg <cjmg@gmail.com>
2025-05-05 22:29:44 +0800

    yeah

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,20 +5,24 @@ const Hero = () => {
         <div className=" ">
             <div className="flex items-center  max-w-[1280px] mx-auto">
                 <div className="flex-1">
-                    <div className="w-[85%]">
-                        <h1 className="text-[3.25rem] font-inter-bold">
+                    <div className="w-[85%] gap">
+                        <h1 className="text-[3.25rem]/14 font-inter-bold mb-6">
                             Your Trusted <br /> Partner for Printing Solutions
                         </h1>
-                        <p className="text-xl font-inter-light">
+                        <p className="text-xl font-inter-light mb-16 text-gray-500">
                             At CJE, we specialize in top-notch printer sales and
                             services, ensuring your printing needs are met with
                             precision and care. Additionally, our expert team is
                             here to handle all your electronic repair needs,
                             providing reliable solutions you can count on.
                         </p>
-                        <div>
-                            <button className="">Get Started</button>
-                            <button>Learn More</button>
+                        <div className="flex gap-6">
+                            <button className="text-primary font-inter-regular cursor-pointer">
+                                Get Started
+                            </button>
+                            <button className="bg-primary text-white px-4 py-2 cursor-pointer rounded-md">
+                                Learn More
+                            </button>
                         </div>
                     </div>
                 </div>
