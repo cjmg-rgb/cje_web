e764e5dcda8915f748f02eb9e15cfed158435933
cjmg <cjmg@gmail.com>
2025-05-06 12:22:17 +0800

    hays

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Feature from "./components/Feature";
+import Testimonials from "./components/Testimonials";
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
             <Hero />
             <About />
             <Feature />
+            <Testimonials />
             <Footer />
         </div>
     );
---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,158 @@
+import React from "react";
+import Star from "../assets/star.svg";
+
+const Testimonials = () => {
+    return (
+        <div className="mb-20">
+            <div className="max-w-[1280px] mx-auto">
+                <div>
+                    <div className="flex flex-col mb-10 gap-2">
+                        <h2 className="text-[2rem] font-inter-bold">
+                            Customer Testimonials
+                        </h2>
+                        <p className="text-lg font-inter-light">
+                            CJE's service transformed our printing experience!
+                        </p>
+                    </div>
+                    <div className="flex items-center gap-20">
+                        <div className="flex flex-col gap-5">
+                            <div className="flex flex-row">
+                                <svg
+                                    height="512px"
+                                    viewBox="0 0 512 512"
+                                    width="512px"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-10 w-10 fill-primary"
+                                >
+                                    <polygon points="448,208 301,208 256,64 211,208 64,208 183.1,297.3 136,448 256,352 376,448 328.9,297.3 " />
+                                </svg>
+                                <svg
+                                    height="512px"
+                                    viewBox="0 0 512 512"
+                                    width="512px"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-10 w-10 fill-primary"
+                                >
+                                    <polygon points="448,208 301,208 256,64 211,208 64,208 183.1,297.3 136,448 256,352 376,448 328.9,297.3 " />
+                                </svg>
+                                <svg
+                                    height="512px"
+                                    viewBox="0 0 512 512"
+                                    width="512px"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-10 w-10 fill-primary"
+                                >
+                                    <polygon points="448,208 301,208 256,64 211,208 64,208 183.1,297.3 136,448 256,352 376,448 328.9,297.3 " />
+                                </svg>
+                                <svg
+                                    height="512px"
+                                    viewBox="0 0 512 512"
+                                    width="512px"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-10 w-10 fill-primary"
+                                >
+                                    <polygon points="448,208 301,208 256,64 211,208 64,208 183.1,297.3 136,448 256,352 376,448 328.9,297.3 " />
+                                </svg>
+                                <svg
+                                    height="512px"
+                                    viewBox="0 0 512 512"
+                                    width="512px"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-10 w-10 fill-primary"
+                                >
+                                    <polygon points="448,208 301,208 256,64 211,208 64,208 183.1,297.3 136,448 256,352 376,448 328.9,297.3 " />
+                                </svg>
+                            </div>
+                            <p className="font-inter-light-italic">
+                                "The team at CJE is incredibly responsive and
+                                helpful! Their expertise in printers is
+                                unmatched."
+                            </p>
+                            <div className="flex gap-4">
+                                <img
+                                    src="https://placehold.co/50x50"
+                                    alt=""
+                                    className="rounded-full"
+                                />
+                                <div>
+                                    <p className="font-inter-light">John Doe</p>
+                                    <p className="text-sm text-gray-500 font-inter-light">
+                                        Manager, TechCorp
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-5">
+                            <div className="flex flex-row">
+                                <svg
+                                    height="512px"
+                                    viewBox="0 0 512 512"
+                                    width="512px"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-10 w-10 fill-primary"
+                                >
+                                    <polygon points="448,208 301,208 256,64 211,208 64,208 183.1,297.3 136,448 256,352 376,448 328.9,297.3 " />
+                                </svg>
+                                <svg
+                                    height="512px"
+                                    viewBox="0 0 512 512"
+                                    width="512px"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-10 w-10 fill-primary"
+                                >
+                                    <polygon points="448,208 301,208 256,64 211,208 64,208 183.1,297.3 136,448 256,352 376,448 328.9,297.3 " />
+                                </svg>
+                                <svg
+                                    height="512px"
+                                    viewBox="0 0 512 512"
+                                    width="512px"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-10 w-10 fill-primary"
+                                >
+                                    <polygon points="448,208 301,208 256,64 211,208 64,208 183.1,297.3 136,448 256,352 376,448 328.9,297.3 " />
+                                </svg>
+                                <svg
+                                    height="512px"
+                                    viewBox="0 0 512 512"
+                                    width="512px"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-10 w-10 fill-primary"
+                                >
+                                    <polygon points="448,208 301,208 256,64 211,208 64,208 183.1,297.3 136,448 256,352 376,448 328.9,297.3 " />
+                                </svg>
+                                <svg
+                                    height="512px"
+                                    viewBox="0 0 512 512"
+                                    width="512px"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-10 w-10 fill-primary"
+                                >
+                                    <polygon points="448,208 301,208 256,64 211,208 64,208 183.1,297.3 136,448 256,352 376,448 328.9,297.3 " />
+                                </svg>
+                            </div>
+                            <p className="font-inter-light-italic">
+                                "I couldn't be happier with the repair service I
+                                received. CJE saved my printer and my business!"
+                            </p>
+                            <div className="flex gap-4">
+                                <img
+                                    src="https://placehold.co/50x50"
+                                    alt=""
+                                    className="rounded-full"
+                                />
+                                <div>
+                                    <p className="font-inter-light">John Doe</p>
+                                    <p className="text-sm text-gray-500 font-inter-light">
+                                        Manager, TechCorp
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Testimonials;
